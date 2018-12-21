8d5a0f94577af88df77deb44ce62341e8b1c7442
fentya <46046378+fentya@users.noreply.github.com>
2018-12-21 19:20:06 -0400

    Create devinette.js
    renommage valeur en CamelCase

---
+++ devinette.js
@@ -0,0 +1,64 @@
+/* 
+Activité : jeu de devinette
+*/
+
+// NE PAS MODIFIER OU SUPPRIMER LES LIGNES CI-DESSOUS
+// COMPLETEZ LE PROGRAMME UNIQUEMENT APRES LE TODO
+
+console.log("Bienvenue dans ce jeu de devinette !");
+
+// Cette ligne génère aléatoirement un nombre entre 1 et 100
+var solution = Math.floor(Math.random() * 100) + 1;
+
+// Décommentez temporairement cette ligne pour mieux vérifier le programme
+//console.log("(La solution est " + solution + ")");
+
+// TODO : complétez le programme
+
+// initialisation
+var proposition=-solution;
+var essai=0;
+
+var essaiMax=6; //nombre maximum d'essai
+
+ 
+loopWhile:
+do {             
+            proposition = Number(prompt("Entrez un nombre entre 1 et 100:"));
+
+            if (proposition<solution) 
+            {
+                console.log(proposition+" est trop petit.");
+            }
+    
+            else if (proposition>solution)
+            {
+                console.log(proposition+" est trop grand.");
+            }
+    
+            else if (Number.isNaN(proposition)||proposition<1||proposition>100)
+            {
+                alert("Attention ! Veuillez entrer un nombre.");               
+            }
+    
+            if (proposition<1||proposition>100)
+            {
+                alert("Attention ! Veuillez entrer un nombre compris entre 1 et 100.");
+                
+            }
+
+    essai++;
+        
+    
+} while(essai<essaiMax &&  proposition!==solution);
+
+if(proposition===solution)
+{
+   console.log("Génial ! La solution est "+solution+".");
+}
+
+else
+{
+     console.log("Perdu!! ... La solution est "+solution+".");
+} 
+
